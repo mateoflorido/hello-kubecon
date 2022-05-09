eca3c94cfddff6f64912ea7f5ba9d03bd6b2bcd0
Mateo Florido <mateo.florido@canonical.com>
2022-05-09 11:53:48 -0500

    Config changed event created

--- src/charm.py
+++ src/charm.py
@@ -61,19 +61,26 @@ class HelloKubeconCharm(CharmBase):
         self.unit.status = ActiveStatus()
 
     def _on_config_changed(self, _):
-        """Just an example to show how to deal with changed configuration.
-
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle config, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the config.py file.
-
-        Learn more about config at https://juju.is/docs/sdk/config
-        """
-        current = self.config["thing"]
-        if current not in self._stored.things:
-            logger.debug("found a new thing: %r", current)
-            self._stored.things.append(current)
+        """Handle the config-changed event"""
+        # Get the gosherve container so we can configure/manipulate it
+        container = self.unit.get_container("gosherve")
+        # Create a new config layer
+        layer = self._gosherve_layer()
+        # Get the current config
+        services = container.get_plan().to_dict().get("services", {})
+        # Check if there are any changes to services
+        if services != layer["services"]:
+            container.add_layer("gosherve", layer, combine=True)
+            logger.info("Added updated layer 'gosherve' to Pebble plan")
+            # Stop the service if it is already running
+            if container.get_service("gosherve").is_running():
+                container.stop("gosherve")
+            # Restart it and report a new status to Juju
+            container.start("gosherve")
+            logger.info("Restarted gosherve service")
+        # All is well, set an ActiveStatus
+        self.unit.status = ActiveStatus()
+            
 
     def _on_fortune_action(self, event):
         """Just an example to show how to receive actions.
@@ -90,6 +97,24 @@ class HelloKubeconCharm(CharmBase):
             event.fail(fail)
         else:
             event.set_results({"fortune": "A bug in the code is worth two in the documentation."})
+    def _gosherve_layer(self):
+        """ Returns a Pebble configuration layer for Gosherve"""
+        return {
+            "summary": "gosherve layer",
+            "description": "pebble config layer for gosherve",
+            "services": {
+                "gosherve": {
+                    "override": "replace",
+                    "summary": "gosherve",
+                    "command": "/gosherve",
+                    "startup": "enabled",
+                    "environment": {
+                        "REDIRECT_MAP_URL": self.config["redirect_map"]
+                    },
+                }
+            },
+        }
+
 
 
 if __name__ == "__main__":
